4aae02974e79b44287ad81d24f701993326f9769
AlexPaturov <alexey.paturov@gmail.com>
2024-10-16 12:52:02 +0300

    Рабочий пример 01

--- DataAccess/GetUserRights.cs
+++ DataAccess/GetUserRights.cs
@@ -2,28 +2,12 @@
 {
     public class GetUserRights
     {
-        public async Task<bool> CheckDomainMetinvest(string domainName) 
-        {
-            // do some staff
-            // execution time approximately 2000 miliseconds
-            await Task.Delay(2000); // emulate execution delay
-            return true;
-        }
-
-        public async Task<bool> CheckLdapAccess(string ldapURL)
-        {
-            // do some staff
-            // execution time approximately 3000 miliseconds
-            await Task.Delay(3000); // emulate execution delay
-            return true;
-        }
-
         public async Task<bool> CheckPcRights(string pcDomainName) 
         {
             // do some staff
             // execution time approximately 5000 miliseconds
             await Task.Delay(5000); // emulate execution delay
-            return false;
+            return true;
         }
 
         public async Task<bool> CheckUserRights(string userDomainName)
